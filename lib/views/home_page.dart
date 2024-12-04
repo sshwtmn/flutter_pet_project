675be6783057d96eecf323d771c8eb6a0b0473d3
Wittmann <ssh.wtmn@gmail.com>
2024-12-04 17:45:23 +0200

    second-commit

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_pet_project/firebase_options.dart';
 import 'package:my_pet_project/views/login_page.dart';
+import 'package:my_pet_project/views/verification_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,13 +17,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              // if (user?.emailVerified ?? false) {
-              //   return const Text('Done');
-              // } else {
-              //   return const LoginView();
-              // }
-              return const LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                if (user.emailVerified){
+                  print('Email is verified');
+                }else {
+                  return const VerifyEmailWidget();
+                }
+              }else {
+                return const LoginView();
+              }
+              return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
